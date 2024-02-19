141502555178c9dcc860018220228fc37f1c16b0
Ludvig Hult <ludvig.hult@it.uu.se>
2024-02-19 11:20:05 +0100

    typo

--- random_arena_games.sh
+++ random_arena_games.sh
@@ -1,6 +1,6 @@
 set -e
 cargo build --release --bin arena
-rm -f score.txt
+rm -f score.csv
 
 players=("random" "mcts1" "mcts2" "mcts3" "minimax4" "ab6");
 for k in {1..1000}; do
@@ -9,6 +9,6 @@ for k in {1..1000}; do
     while [ $i -eq $j ]; do
         j=$((RANDOM % ${#players[@]}))
     done
-    ./target/release/arena --outfile score.txt --game c4 run  --player2 ${players[$j]} --player1 ${players[$i]} ;
-    ./target/release/arena --outfile score.txt --game c4 report
+    ./target/release/arena --outfile score.csv --game c4 run  --player2 ${players[$j]} --player1 ${players[$i]} ;
+    ./target/release/arena --outfile score.csv --game c4 report
 done;
